a5248ead776f1d8f8fa87b2f32fa5ff613b69558
olojedesuliatcodes <temitayoabiodun15@gmail.com>
2025-03-14 04:51:09 -0700

    numeric python

---
+++ 003_numeric_data.py
@@ -0,0 +1,29 @@
+int_var = 30
+float_var = 6.75
+
+print(int_var + float_var)
+print(int_var - float_var)
+print(int_var * float_var)
+print(int_var / float_var)
+print(int_var // float_var)
+print(int_var % float_var)
+print(int_var ** float_var)
+
+result = (10 + 3) * 2 ** 2 / 4 - 1
+print(result)
+
+print(abs(-15))
+
+print(round(float_var, 1))
+print(round(float_var, 2))
+
+print(int_var == float_var)
+print(int_var != float_var)
+print(int_var > float_var)
+print(int_var < float_var)
+print(int_var <= float_var)
+print(int_var >= float_var)
+
+print(int(float_var))
+print(float(int_var))
+print(int("20"))

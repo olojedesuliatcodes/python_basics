58b3159c70c49538cf104c7772c99cd1db48a42a
olojedesuliatcodes <temitayoabiodun15@gmail.com>
2025-04-03 21:22:42 -0700

    python break

---
+++ 021_password_validation.py
@@ -0,0 +1,9 @@
+password = "python123"
+while True:
+      userinput = input("Enter password")
+      if userinput == password:
+         print("Access Granted")
+else:
+         print("wrong password, try again")
+
+
---
+++ 023_break.py
@@ -0,0 +1,4 @@
+for num in range(1,11):
+     print(num)
+     if num == 5:
+         break

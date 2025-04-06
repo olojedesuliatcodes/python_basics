73f7e311e7944233cd3d5d1af2450cc58c857605
olojedesuliatcodes <temitayoabiodun15@gmail.com>
2025-04-05 21:46:30 -0700

    function lessons

---
+++ 026_function.py
@@ -0,0 +1,13 @@
+def greet_user():
+    print("Hello, welcome to Python programming!")
+
+def add_numbers(a, b):
+    print("The sum:", a + b)
+
+add_numbers(2, 5)
+
+def multiply_numbers(a, b):
+    return a * b
+
+result = multiply_numbers(2, 5)
+print("The product:", result)
